710954ef6a32f1e351da6954946881571ec89bac
Stephan Dowless <dowless.stephan@gmail.com>
2021-08-27 15:22:10 -0400

    Initial Commit

---
+++ NetworkManagerSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetworkManagerSwiftUI
+//
+//  Created by Stephen Dowless on 8/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkManagerSwiftUI/NetworkManagerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManagerSwiftUIApp.swift
+//  NetworkManagerSwiftUI
+//
+//  Created by Stephen Dowless on 8/27/21.
+//
+
+import SwiftUI
+
+@main
+struct NetworkManagerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
